a78e5e200b36ddd87047ed8ffe9111d230d9bfec
tugbaekee <tugbaeke.402@gmail.com>
2025-05-21 17:59:48 +0300

    Add working frame slider integration with Mol* viewer
    - Integrated Dash `dcc.Slider` to control trajectory frame index dynamically
    - Replaced individual frame loading with full trajectory preload via `molstar_helper.get_trajectory`
    - Updated callback to adjust MolstarViewer 'frame' property using slider value

--- test.py
+++
@@ -1,7 +0,0 @@
-import dash, os
-from dash import dcc, html, dash_table, Input, Output
-import dash_bio as dashbio
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-from dash_bio.utils import PdbParser, create_mol3d_style
